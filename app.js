e4cfc0f7635080c32a023c469a3bd9bafbfae4af
Fredrik Lillejordet <fretholil@gmail.com>
2015-10-12 17:05:28 +0200

    Fungerer, ish

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var server = require('http').createServer();
 var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({ server: server });
+var wss = new WebSocketServer({
+    server: server
+});
 var express = require('express');
 var app = express();
 var port = 8000;
@@ -9,76 +11,78 @@ app.use('/', express.static(__dirname + '/'));
 
 var idCounter = 0;
 
-var players = [ ];
+var players = [];
 
 wss.on('connection', function(ws) {
-	ws.playerID = idCounter++;
-	ws.pos = {
-		x: 0,
-		y: 0,
-		z: 0
-	};
-	
-	for (var i = 0; i < players.length; i++) {
-		players[i].send(JSON.stringify({
-			"type": "newPlayer",
-			"id": ws.playerID
-		}));
-	}
-	
-	players.push(ws);
-	
-	ws.on('message', function(message) {
-		var msg = JSON.parse(message);
-		if (msg.type === 'pos') {
-			ws.pos = msg.pos;
-		}
-	});
-	
-	ws.on('close', function(reason) {
-		for (var i = 0; i < players.length; i++) {
-			if (players[i] === ws) {
-				players.splice(i, 1);
-				break;
-			}
-		}
-	});
-	
-	ws.send(JSON.stringify({
-		"type": "ready",
-		"id": ws.playerID
-	}));
+    ws.playerID = idCounter++;
+    ws.pos = {
+        x: 0,
+        y: 0,
+        z: 0
+    };
+
+    for (var i = 0; i < players.length; i++) {
+        players[i].send(JSON.stringify({
+            "type": "newPlayer",
+            "playerID": ws.playerID
+        }));
+    }
+
+    players.push(ws);
+
+    ws.on('message', function(message) {
+        var msg = JSON.parse(message);
+        if (msg.type === 'pos') {
+            ws.pos = msg.pos;
+        }
+    });
+
+    ws.on('close', function(reason) {
+        for (var i = 0; i < players.length; i++) {
+            if (players[i] === ws) {
+                players.splice(i, 1);
+                break;
+            }
+        }
+    });
+
+    ws.send(JSON.stringify({
+        "type": "ready",
+        "id": ws.playerID
+    }));
 });
 
 
 var sendPositions = function() {
-	var positions = JSON.stringify(getPlayerPositions());
-	for (var i = 0; i < players.length; i++) {
-		players[i].send(JSON.stringify({
-			"type": "pos",
-			"pos": positions
-		}));
-	}
+    var positions = JSON.stringify(getPlayerPositions());
+    for (var i = 0; i < players.length; i++) {
+        players[i].send(JSON.stringify({
+            "type": "pos",
+            "pos": positions
+        }));
+    }
 };
 
 var getPlayerPositions = function() {
-	var playerPositions = [ ];
-	for (var i = 0; i < players.length; i++) {
-		playerPositions.push({
-			id: players[i].playerID,
-			pos: {
-				x: players[i].pos.x,
-				y: players[i].pos.y,
-				z: players[i].pos.z
-			}
-		});
-	}
-	return playerPositions;
+    var playerPositions = [];
+    for (var i = 0; i < players.length; i++) {
+        playerPositions.push({
+            id: players[i].playerID,
+            pos: {
+                x: players[i].pos.x,
+                y: players[i].pos.y,
+                z: players[i].pos.z
+            }
+        });
+    }
+    return playerPositions;
 };
 
 setInterval(function() {
-	sendPositions();
+    sendPositions();
 }, 100);
 
 server.on('request', app);
-server.listen(port, function () { console.log('Listening on ' + server.address().port) });
+server.listen(port, function() {
+    console.log('Listening on ' + server.address().port)
+});
